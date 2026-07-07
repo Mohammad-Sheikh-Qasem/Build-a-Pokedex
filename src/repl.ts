9c5b37a35091b75871b7c56a1366584aacc06e08
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-07 16:09:57 +0300

    complete the code

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,31 @@
+import readline from "readline";
+
 export function cleanInput(input: string): string[] {
-  return input.trim().split(/\s+/);
+  return input.trim().toLowerCase().split(/\s+/);
+}
+
+export function startREPL(
+  input: NodeJS.ReadableStream,
+  output: NodeJS.WritableStream,
+) {
+  const rl = readline.createInterface({
+    input,
+    output,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", (input: string) => {
+    const words = cleanInput(input);
+
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${words[0]}`);
+
+    rl.prompt();
+  });
 }
