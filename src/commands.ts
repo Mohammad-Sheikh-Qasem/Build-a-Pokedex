cc428660ae4a292bf0ebfd94ecd82327535e5268
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-08 14:20:21 +0300

    refactor: update commands to use state object

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,8 @@
 import type { CLICommand } from "./state.js";
 import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./command_help.js";
+import { commandMap } from "./command_map.js";
+import { commandMapBack } from "./command_mapb.js";
 
 export function getCommands(): Record<string, CLICommand> {
   return {
@@ -9,10 +11,23 @@ export function getCommands(): Record<string, CLICommand> {
       description: "Exit the Pokedex",
       callback: commandExit,
     },
+
     help: {
       name: "help",
       description: "Displays a help message",
       callback: commandHelp,
+    },
+
+    map: {
+      name: "map",
+      description: "Displays the next 20 locations",
+      callback: commandMap,
+    },
+
+    mapb: {
+      name: "mapb",
+      description: "Displays the previous 20 locations",
+      callback: commandMapBack,
     },
   };
 }
