0fe02f7273fec547900790aa56f11a76dce46938
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-08 15:07:43 +0300

    add pokemon and location response types

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -13,7 +13,25 @@ export type ShallowLocations = {
 export type Pokemon = {
   name: string;
   base_experience: number;
+
+  height: number;
+  weight: number;
+
+  stats: {
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }[];
+
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
 };
+
+
 export type Location = {
   pokemon_encounters: {
     pokemon: {
