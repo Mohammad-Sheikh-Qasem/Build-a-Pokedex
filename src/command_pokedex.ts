be5e30a01e2216a332cce3870907f8b7dc33dfec
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-08 15:18:40 +0300

    implement pokedex command

---
+++ src/command_pokedex.ts
@@ -0,0 +1,12 @@
+import type { State } from "./state.js";
+
+export async function commandPokedex(
+  state: State
+): Promise<void> {
+
+  console.log("Your Pokedex:");
+
+  for (const pokemonName in state.pokedex) {
+    console.log(` - ${pokemonName}`);
+  }
+}
