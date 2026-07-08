3499f1e193e9aec04ee6ed9549499394a8d666e5
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-08 13:56:08 +0300

    update the code

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,10 +1,11 @@
-import type { CLICommand } from "./command.js";
+import type { State } from "./state.js";
 
-export function commandHelp(commands: Record<string, CLICommand>) {
+export function commandHelp(state: State) {
   console.log("Welcome to the Pokedex!");
-  console.log("Usage:\n");
 
-  for (const command of Object.values(commands)) {
+  console.log("Usage:");
+
+  for (const command of Object.values(state.commands)) {
     console.log(`${command.name}: ${command.description}`);
   }
 }
